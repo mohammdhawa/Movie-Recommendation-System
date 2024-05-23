12cf40f793f3241e3c390ef87ba9bfa8891d5736
Muhammed Recep Hava <ragab5434@gmail.com>
2024-05-23 14:29:06 +0300

    design and create movie, Actor, Genre models(DB)

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,76 @@
+import os
+import requests
+from django.core.files.base import ContentFile
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genres = models.ManyToManyField('Genre', related_name='movie_genres')
+    production_country = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    runtime = models.IntegerField()
+    release_date = models.DateField()
+    language = models.CharField(max_length=255)
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
+    image = models.ImageField(upload_to='movies/', blank=True, null=True)
+    actors = models.ManyToManyField('Actor', related_name='movie_actors')
+    trailer = models.URLField(blank=True, null=True)
+    overview = models.TextField(blank=True, null=True)
+
+    movie_id = models.IntegerField(blank=True, null=True)
+    tmdb_id = models.IntegerField(blank=True, null=True)
+    imdb_id = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_image_from_url(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            # Extracting file name from URL
+            file_name = url.split('/')[-1]
+            # Setting the path where the image will be saved
+            file_path = os.path.join(settings.MEDIA_ROOT, 'movies', file_name)
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+            # Saving image to the ImageField
+            self.image.save(file_name, ContentFile(response.content), save=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='actors/', blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)  # New field to store the URL of the image
+
+    def __str__(self):
+        return self.name
+
+    def save_image_from_url(self):
+        if self.image_url:
+            response = requests.get(self.image_url)
+            if response.status_code == 200:
+                # Save the downloaded image to the media directory
+                media_root = settings.MEDIA_ROOT
+                image_name = os.path.basename(self.image_url)
+                image_path = os.path.join(media_root, "actors", image_name)
+                with open(image_path, "wb") as f:
+                    f.write(response.content)
+
+                # Set the image field to the path of the saved image
+                self.image = os.path.join("actors", image_name)
+
+                # Save the model
+                self.save()
+
+                return True
+        return False
---
+++ recommendation_logic.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import pickle
+import requests
+
+import django
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+
+path = "content_based.pkl"
+
+# Load data from pickle file
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+
+with open('actors_ids.pkl', 'rb') as f:
+    actors_data = pickle.load(f)
+
+# Access loaded data
+actors_ids = actors_data['actors_ids']
+movies_ = data['movies']
+movies_filter_content_based = data['movies_filter_content_based']
+final_movies = data['final_movies']
+
+
+# Define the recommend_movies4 function
+def recommend_movies4(movie_title):
+    movie_id = movies_filter_content_based.loc[movies_filter_content_based['title'] == movie_title, 'id'].values
+    movie_id = movie_id[0]
+
+    movie_row = movies_filter_content_based[movies_filter_content_based['id'] == movie_id]
+
+    if movie_row.empty:
+        raise RuntimeError(f"Couldn't find any movie with ID: {movie_id}")
+
+    movie_row = movie_row.iloc[0]
+
+    director = movie_row['movie_director']
+    actors = [movie_row[f'actor_{i}'] for i in range(1, 4)]
+    genres = [movie_row[f'genre_{i}'] for i in range(1, 4)]
+
+    keywords = [movie_row[f'keyword_{i}'] for i in range(1, 6)]
+    rec_df = movies_filter_content_based.copy()
+
+    rec_df['same_director'] = (rec_df['movie_director'] == director).astype(int)
+
+    for i, actor in enumerate(actors):
+        rec_df[f'same_a{i + 1}'] = ((rec_df['actor_1'] == actor) |
+                                    (rec_df['actor_2'] == actor) |
+                                    (rec_df['actor_3'] == actor)).astype(int)
+
+    for i, genre in enumerate(genres):
+        rec_df[f'same_g{i + 1}'] = ((rec_df['genre_1'] == genre) |
+                                    (rec_df['genre_2'] == genre) |
+                                    (rec_df['genre_3'] == genre)).astype(int)
+
+    for i, keyword in enumerate(keywords):
+        rec_df[f'same_k{i + 1}'] = ((rec_df['keyword_1'] == keyword) |
+                                    (rec_df['keyword_2'] == keyword) |
+                                    (rec_df['keyword_3'] == keyword) |
+                                    (rec_df['keyword_4'] == keyword) |
+                                    (rec_df['keyword_5'] == keyword)).astype(int)
+
+    rec_df.loc[:, 'same_director': 'same_k5'] = rec_df.loc[:, 'same_director': 'same_k5'].fillna(0)
+    rec_df['sim_count'] = rec_df.loc[:, 'same_director': 'same_k5'].sum(axis=1)
+    rec_df = rec_df.merge(movies_[['id', 'weighted_rating']], on='id')
+
+    # Select top 5 recommended movies based on similarity count and weighted rating
+    top5_rec = rec_df.sort_values(by=['sim_count', 'weighted_rating'], ascending=[False, False]).iloc[:5]
+
+    return list(top5_rec['id'])
+
+
+def get_movie_detail(movie_id):
+    result = final_movies[final_movies['id'] == movie_id]
+    if not result.empty:
+        return dict(result.iloc[0])
+    else:
+        return None
+
+
+def fetch_poster(movie_id):
+    response = requests.get(
+        f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=2646356451a5c6c0d4606b0ccb8a0b0a&language=en-US")
+    "https://api.themoviedb.org/3/movie/557/similar?api_key=2646356451a5c6c0d4606b0ccb8a0b0a&language=en-US&page=1"
+    "https://api.themoviedb.org/3/movie/557/credits?api_key=2646356451a5c6c0d4606b0ccb8a0b0a&language=en-US&page=1"
+    "https://api.themoviedb.org/3/person/210050/images?api_key=2646356451a5c6c0d4606b0ccb8a0b0a&language=en-US"
+    movie_poster = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + movie_poster['poster_path']
+
+
+# Function to get actor_id by actor_name
+def get_actor_id_by_name(name):
+    result = actors_ids.loc[actors_ids['actor'] == name, 'actor_id']
+    if not result.empty:
+        return result.values[0]
+    else:
+        return None
+
+
+def fetch_actor_profile(actor_id):
+    response = requests.get(
+        f"https://api.themoviedb.org/3/person/{actor_id}/images?api_key=2646356451a5c6c0d4606b0ccb8a0b0a&language=en-US")
+    actor_image = response.json()['profiles'][0]['file_path']
+    return "https://image.tmdb.org/t/p/w500/" + actor_image
+
+# mytest = recommend_movies4('Spider-Man')
+# print(fetch_poster(mytest[0]))
+# print(get_movie_detail(mytest[0]))
+# print(mytest)
