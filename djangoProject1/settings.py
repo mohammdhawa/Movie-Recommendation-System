ed60d789f9f1ba219fc82b7a5f09530efb25d1c6
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-06-01 16:45:24 +0300

    created registration & profile pages

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # My Package
+    "bootstrap5",
+    
     # My Apps
     "movies.apps.MoviesConfig",
 ]
--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('movies.urls')),
 ]
